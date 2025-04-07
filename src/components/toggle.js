2fc5b9743196ab197b72434c9da6fd2133fd6ce7
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-07 21:49:14 +0300

    changes

--- src/components/toggle.js
+++ src/components/toggle.js
@@ -1,2 +1,9 @@
 import { useState, useEffect } from "react";
 import './toggle.css';
+
+function toggle(){
+    const [isToggled, setIsToggled] = useState(false);
+
+    const toggle = () => {
+        setIsToggled(!isToggled);
+}
